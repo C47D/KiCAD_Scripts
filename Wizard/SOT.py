e3a425d2f7f00b1c01cbde856331bc8359503638
C47D <carlos.santiago.diaz@gmail.com>
2016-07-24 14:01:41 -0500

    Reference and soldering lead added

--- Wizard/SOT.py
+++ Wizard/SOT.py
@@ -18,7 +18,6 @@ from __future__ import division
 import pcbnew
 
 import HelpfulFootprintWizardPlugin as HFWP
-import FootprintWizardDrawingAids as FWDA
 import PadArray_Thm as PA
 
 class SOT_Wizard(HFWP.HelpfulFootprintWizardPlugin):
@@ -31,91 +30,119 @@ class SOT_Wizard(HFWP.HelpfulFootprintWizardPlugin):
 
     def GenerateParameterList(self):
         # Tab Pads, opcion n, recibe numeros naturales, valor por defecto = 3
+	self.AddParam("Pads", "Family: SOT23, SOT89, SOT143, SOT223, SOTFL", self.uString, 'SOT223')
         self.AddParam("Pads", "n", self.uNatural, 3)
 	self.AddParam("Pads", "Pitch", self.uMM, 0.2)        
-        self.AddParam("Pads", "pad width", self.uMM, 0.3)
-        self.AddParam("Pads", "pad length", self.uMM, 0.9)
-        self.AddParam("Pads", "vertical pitch", self.uMM, 8.9)
-        self.AddParam("Pads", "horizontal pitch", self.uMM, 8.9)
-        self.AddParam("Pads", "oval", self.uBool, True)
-        self.AddParam("Pads", "package width", self.uMM, 9)
-        self.AddParam("Pads", "package height", self.uMM, 9)
-        self.AddParam("Pads", "epad width", self.uMM, 7.5)
-        self.AddParam("Pads", "epad length", self.uMM, 7.5)
-        self.AddParam("Pads", "epad subdivision h", self.uNatural, 2)
-        self.AddParam("Pads", "epad subdivision v", self.uNatural, 2)
-        self.AddParam("Pads", "epad thermal h pitch", self.uMM, 1.0)
-        self.AddParam("Pads", "epad thermal v pitch", self.uMM, 1.0)
-        self.AddParam("Pads", "epad solder paste ratio", self.uNatural, 0.8)
-        self.AddParam("Pads", "epad thermal via diam", self.uMM, 0.3)
-        self.AddParam("Pads", "epad thermal via size", self.uMM, 0.5)
+        self.AddParam("Pads", "Pad width", self.uMM, 0.3)
+        self.AddParam("Pads", "Pad length", self.uMM, 0.9)
+        self.AddParam("Pads", "Vertical pitch", self.uMM, 8.9)
+        self.AddParam("Pads", "Horizontal pitch", self.uMM, 8.9)
+        self.AddParam("Pads", "Oval", self.uBool, True)
+        self.AddParam("Pads", "Package width", self.uMM, 9)
+        self.AddParam("Pads", "Package height", self.uMM, 9)
+
         # Tab Naming
         self.AddParam("FootprintName", "Prefix", self.uString, 'SOT')
 	# IPC
-	self.AddParam("IPC", "Density, M (Most), N(Nominal), L(Least)", self.uString, 'N')
+	self.AddParam("IPC", "Density, M(Most), N(Nominal), L(Least)", self.uString, 'N')
+	# Silkscreen pin 1 mark
+	self.AddParam("IPC", "Pin 1 mark, c(circle), l(line)", self.uString, 'l')
 
     def CheckParameters(self):
         self.CheckParamInt("Pads", "*n", is_multiple_of = 1)
-        self.CheckParamBool("Pads", "*oval")
+        self.CheckParamBool("Pads", "*Oval")
 
     def GetValue(self):
-	_density = self.parameters["IPC"]["*Density, M (Most), N(Nominal), L(Least)"]
-	"""
-	if _density == 'N':
-		_toe = 0.20
-		_heel = 0.00
-		_side = 0.00
-		_roundOff = 0.1
-		_courtyard = 0.15
-	else if _density == 'M':
-		_toe = 0.30
-		_heel = 0.00
-		_side = 0.05
-		_roundOff = 0.1
-		_courtyard = 0.20
-	else if _density == 'L':
-		_toe = 0.10
-		_heel = 0.00
-		_side = -0.05
-		_roundOff = 0.1
-		_courtyard = 0.12
-	"""
+	_density = self.parameters["IPC"]["*Density, M(Most), N(Nominal), L(Least)"]
+
 	_prefix = self.parameters["FootprintName"]["*Prefix"]
 	_padNo = self.parameters["Pads"]["*n"]
 	_pitch = self.parameters["Pads"]["Pitch"] / 10000
-	_width = self.parameters["Pads"]["package width"] / 10000
-	_height = self.parameters["Pads"]["package height"] / 10000
+	_width = self.parameters["Pads"]["Package width"] / 10000
+	_height = self.parameters["Pads"]["Package height"] / 10000
         return "%s%dP%dX%d%d-%s" % (_prefix, _pitch, _width, _height, _padNo, _density)
 
+    def DrawOriginGravityCenter(self):
+
+	# Draw the Origin of Gravity Center circle and cross lines.
+        self.draw.SetLayer(pcbnew.F_Fab)
+	_thickness = pcbnew.FromMM(0.05)
+	self.draw.SetLineThickness(_thickness)
+	# Drawing the circle in ( 0 , 0 ) with radius 0.25 mm
+	self.draw.Circle(0, 0, (pcbnew.FromMM(0.25) - (_thickness / 2)))
+	# Drawing vertical line
+	self.draw.Line(0, - pcbnew.FromMM(0.35), 0, pcbnew.FromMM(0.35))
+	# Draw horizontal line
+	self.draw.Line(- pcbnew.FromMM(0.35), 0, pcbnew.FromMM(0.35), 0)
+
+    def CalculatePadDimentions(self):
+        print "Que tranza prros"
+	
+	_family = self.parameters["Pads"]["*Family: SOT23, SOT89, SOT143, SOT223, SOTFL"]
+	_density = self.parameters["IPC"]["*Density, M(Most), N(Nominal), L(Least)"]
+	
+	if _family in ['SOTFL', 'SOT89'] and _density == 'N':
+
+		if _density == 'N':
+			_toe = 0.20
+			_heel = 0.00
+			_side = 0.00
+			_roundOff = 0.1
+			_courtyard = 0.15
+		elif _density == 'M':
+			_toe = 0.30
+			_heel = 0.00
+			_side = 0.05
+			_roundOff = 0.1
+			_courtyard = 0.20
+		elif _density == 'L':
+			_toe = 0.10
+			_heel = 0.00
+			_side = -0.05
+			_roundOff = 0.1
+			_courtyard = 0.12
+	else:
+		if _density == 'N':
+			_toe = 0.35
+			_heel = 0.35
+			_side = 0.03
+			_roundOff = 0.1
+			_courtyard = 0.25
+		elif _density == 'M':
+			_toe = 0.55
+			_heel = 0.45
+			_side = 0.05
+			_roundOff = 0.1
+			_courtyard = 0.50
+		elif _density == 'L':
+			_toe = 0.15
+			_heel = 0.25
+			_side = 0.01
+			_roundOff = 0.1
+			_courtyard = 0.12
+
+
     def BuildThisFootprint(self):
+	
+	self.DrawOriginGravityCenter()
+	self.CalculatePadDimentions()
+
         pads = self.parameters["Pads"]
 	name = self.parameters["FootprintName"]
 
         module_prefix = name["*Prefix"]
         pad_pitch = pads["Pitch"]
-        pad_length = self.parameters["Pads"]["pad length"]
-        pad_width = self.parameters["Pads"]["pad width"]
+        pad_length = self.parameters["Pads"]["Pad length"]
+        pad_width = self.parameters["Pads"]["Pad width"]
 
-        epad_length = self.parameters["Pads"]["epad length"]
-        epad_width = self.parameters["Pads"]["epad width"]
-        epad_subd_l = pads["*epad subdivision h"]
-        epad_subd_w = pads["*epad subdivision v"]
-
-        epad_th_pitch_l = self.parameters["Pads"]["epad thermal h pitch"]
-        epad_th_pitch_w = self.parameters["Pads"]["epad thermal v pitch"]
-        epad_paste_ratio = pads["*epad solder paste ratio"]
-
-        epad_th_via_dm = self.parameters["Pads"]["epad thermal via diam"]
-        epad_th_via_sz = self.parameters["Pads"]["epad thermal via size"]
-
-        v_pitch = pads["vertical pitch"]
-        h_pitch = pads["horizontal pitch"]
+        v_pitch = pads["Vertical pitch"]
+        h_pitch = pads["Horizontal pitch"]
 
         pads_per_row = pads["*n"] // 4
 
         row_len = (pads_per_row - 1) * pad_pitch
 
-        pad_shape = pcbnew.PAD_SHAPE_OVAL if pads["*oval"] else pcbnew.PAD_SHAPE_RECT
+        pad_shape = pcbnew.PAD_SHAPE_OVAL if pads["*Oval"] else pcbnew.PAD_SHAPE_RECT
 
         h_pad = PA.PadMaker(self.module).SMDPad(
             pad_width, pad_length, shape=pad_shape)
@@ -150,45 +177,11 @@ class SOT_Wizard(HFWP.HelpfulFootprintWizardPlugin):
         array.SetFirstPadInArray(3*pads_per_row + 1)
         array.AddPadsToModule(self.draw)
 
-        #epad
-        if epad_length!=0 and epad_width!=0:  #there is exposed pad
-            e_pad = PA.PadMaker(self.module).SMDePad(
-            epad_width, epad_length, shape=pcbnew.PAD_SHAPE_RECT)
-            epadPos = pcbnew.wxPoint (0, 0)
-            array = PA.PadLineArray(e_pad, 1, 1, False,
-                                    epadPos)
-            array.SetFirstPadInArray(pads["*n"]+1)
-            array.AddPadsToModule(self.draw)
-
-            if epad_subd_l!=0 and epad_subd_w!=0 and epad_th_pitch_w!=0 and epad_th_pitch_l!=0:
-            #if we want epad
-                epad_width_part_size_w = epad_width / epad_subd_w  #size of one part of exposed pad
-                epad_width_part_size_l = epad_length / epad_subd_l
-                epad_paste = PA.PadMaker(self.module).SMDPad(
-                epad_width_part_size_w*epad_paste_ratio, epad_width_part_size_l*epad_paste_ratio, shape=pcbnew.PAD_SHAPE_RECT)
-
-                n_epad_parts_w = int(epad_width/epad_th_pitch_w)
-                n_epad_parts_l = int(epad_length/epad_th_pitch_l)
-
-                array_epad = PA.PadThermalArray(epad_paste, epad_subd_l, epad_subd_w,
-                                    epad_width_part_size_l, epad_width_part_size_w, epadPos)
-
-                array_epad.SetFirstPadInArray(pads["*n"]+1)
-                array_epad.AddPadsToModule(self.draw)
-
-                if epad_th_via_sz!=0 and epad_th_via_dm!=0:
-                    #if we want epad thermal vias
-                    epad_th_pad = PA.PadMaker(self.module).THPad(epad_th_via_sz,
-                                epad_th_via_sz, epad_th_via_dm, shape=pcbnew.PAD_SHAPE_CIRCLE)
-                    array_epad_th = PA.PadThermalArray(epad_th_pad, n_epad_parts_l, n_epad_parts_w,
-                                                epad_th_pitch_l, epad_th_pitch_w, epadPos)
-                    array_epad_th.SetFirstPadInArray(pads["*n"]+1)
-                    array_epad_th.AddPadsToModule(self.draw)
-
-        lim_x = pads["package width"] / 2
-        lim_y = pads["package height"] / 2
+        lim_x = pads["Package width"] / 2
+        lim_y = pads["Package height"] / 2
         inner = (row_len / 2) + pad_pitch
 
+        self.draw.SetLayer(pcbnew.F_CrtYd)
         #top left - diagonal
         self.draw.Line(-lim_x, -inner, -inner, -lim_y)
         # top right
